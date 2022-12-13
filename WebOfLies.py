58c6499c68c8c3662df02c2efb4acd156a50be49
Or Peleg <opeleg007@gmail.com>
2022-12-13 15:44:41 +0200

    Implemented reading and unpacking of input.txt

--- WebOfLies.py
+++ WebOfLies.py
@@ -29,14 +29,37 @@ class NobleLinkedList:
         pass
 
 
-def read_input_line_args():
+def unpack_queries_from_line_args(all_queries: list):
+    queries = []
+    for query in all_queries:
+        pack = []
+        for literal in query:
+            pack += [int(literal[0])]
+        queries.append(pack)
+    return queries
+
+
+def read_input_line_args(file_path: str):
+    f = open(file_path, "r")
+    n, m = map(lambda x: int(x), f.readline().split(' '))
+    all_uv = [map(lambda x: int(x), f.readline().split(' ')) for _ in range(m)]
+    friendships = [(u, v) for u, v in all_uv]
+    query_amount = int(f.readline())
+    all_queries = [f.readline().split(' ') for _ in range(query_amount)]
+    queries = unpack_queries_from_line_args(all_queries)
+    f.close()
     return n, m, friendships, queries
 
 
 def init_data_structures(n:int, m:int, frienships:list[tuple], queries:list[tuple]):
-    return noble_linked_list, queries
+    return #noble_linked_list, queries
+
+
+def main():
+    file_path = "input.txt"
+    n, m, friendships, queries = read_input_line_args(file_path)
+    noble_linked_list, queries = init_data_structures(n, m, friendships, queries)
 
 
 if __name__ == '__main__':
-    n, m, friendships, queries = read_input_line_args()
-    noble_linked_list, queries = init_data_structures(n, m, friendships, queries)
+    main()
